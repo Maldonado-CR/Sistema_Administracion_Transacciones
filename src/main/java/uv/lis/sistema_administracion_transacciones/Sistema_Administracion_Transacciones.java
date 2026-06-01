3d60a371f5af0c82ed9653c5098a9f51473bdf99
Maldonado-CR <125850473+Maldonado-CR@users.noreply.github.com>
2026-06-01 16:08:00 -0600

    Primer commit: Estructura inicial del Sistema de Administración de Transacciones

---
+++ src/main/java/uv/lis/sistema_administracion_transacciones/Sistema_Administracion_Transacciones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uv.lis.sistema_administracion_transacciones;
+
+/**
+ *
+ * @author cinth
+ */
+public class Sistema_Administracion_Transacciones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
